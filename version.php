7092dc3d833e8c612bf848070e58bbe9fb50f336
sokunthearithmakara <smakara@hawaii.edu>
2025-04-30 03:14:49 +0700

    2025043002

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_ivxpreward';
 $plugin->release      = '1.1';
-$plugin->version      = 2025041100;
+$plugin->version      = 2025043000;
 $plugin->requires     = 2021112800;
 $plugin->supported    = [400, 500];
 $plugin->maturity     = MATURITY_STABLE;
